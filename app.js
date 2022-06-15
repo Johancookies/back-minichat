efecf90a3605822bae5c2704e2d09004b9be5369
Johancookies <johan.mancilla@bodytechcorp.com>
2022-06-15 13:49:34 -0500

    Basic chat added

--- app.js
+++ app.js
@@ -11,15 +11,23 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
   socket.on("disconnect", () => {
-    console.log("User Disconnected");
+    console.log(`User Disconnected ${socket.id}`);
   });
 });
 
