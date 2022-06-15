c9029b1b53791a41353e9754a7bc0b711593789f
Johancookies <johan.mancilla@bodytechcorp.com>
2022-06-15 10:26:18 -0500

    Connections updated

--- app.js
+++ app.js
@@ -11,18 +11,15 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
   });
 });
 
