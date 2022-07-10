86c4c9f6cbdeb06b1c23cdf34ada9414a1a2077e
EdinNicolas-web <edin_albarracin@outlook.com>
2022-07-10 15:38:59 -0500

    rabbit consume

--- rabbitmq/send.js
+++ rabbitmq/send.js
@@ -8,34 +8,32 @@ const sendMessageRabbit = ({ id_channel, msg, res, queryMySql, }) => {
       const message = Buffer.from(JSON.stringify(msg));
       channel.assertQueue(queue, { durable: true });
       channel.sendToQueue(queue, message, { persistent: true });
-      channel.prefetch(1);
-      queryMySql(msg);
       channel.close(() => {
         conn.close();
       });
     });
   });
-  // rabbitConnect((conn) => {
-  //  conn.createChannel((err, channel) => {
-  //    if (err) { console.log("error") }
-  //    const queue = id_channel;
-  //    channel.assertQueue(queue, { durable: true });
-  //     channel.prefetch(1);
-  //     channel.consume(
-  //      queue,
-  //       function (msg) {
-	//  console.log(msg);
-  //         var buf = JSON.parse(msg.content);
-  //         insert to database
-  //         queryMySql(buf);
-  //       },
-  //       { noAck: true }
-  //     );
-  //    channel.close(() => {
-  //      conn.close();
-  //     });
-  //   });
-  // });
+  rabbitConnect((conn) => {
+   conn.createChannel((err, channel) => {
+     if (err) { console.log("error") }
+     const queue = id_channel;
+     channel.assertQueue(queue, { durable: true });
+      channel.prefetch(1);
+      channel.consume(
+       queue,
+        function (msg) {
+	 console.log(msg);
+          var buf = JSON.parse(msg.content);
+          //insert to database
+          queryMySql(buf);
+        },
+        { noAck: true }
+      );
+     channel.close(() => {
+       conn.close();
+      });
+    });
+  });
 };
 
 export default sendMessageRabbit;
