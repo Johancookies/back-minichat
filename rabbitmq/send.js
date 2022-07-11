6d4468cd97af7d4c0e3402bf64aa490e52593e78
steveen <israel.delarosa@bodytechcorp.com>
2022-07-11 14:52:08 -0500

    fix rabbitmq

--- rabbitmq/send.js
+++ rabbitmq/send.js
@@ -8,7 +8,7 @@ const sendMessageRabbit = ({ id_channel, msg, res, queryMySql }) => {
       if (err) console.log("rror");
       const queue = id_channel;
       const message = Buffer.from(JSON.stringify(msg));
-      channel.assertQueue(queue);
+      channel.assertQueue(queue, { durable: true });
       channel.sendToQueue(queue, message, { persistent: true });
     });
   });
@@ -18,7 +18,7 @@ const sendMessageRabbit = ({ id_channel, msg, res, queryMySql }) => {
         console.log("error");
       }
       const queue = id_channel;
-      channel.assertQueue(queue);
+      channel.assertQueue(queue, { durable: true });
       // channel.prefetch(1);
       channel.consume(
         queue,
