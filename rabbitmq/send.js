acbe1feea77b6f467e3b203511d9fc43ef4b8c70
steveen <israel.delarosa@bodytechcorp.com>
2022-07-11 13:52:32 -0500

    fix rabbitmq

--- rabbitmq/send.js
+++ rabbitmq/send.js
@@ -10,6 +10,15 @@ const sendMessageRabbit = ({ id_channel, msg, res, queryMySql }) => {
       const message = Buffer.from(JSON.stringify(msg));
       channel.assertQueue(queue, { durable: true });
       channel.sendToQueue(queue, message, { persistent: true });
+    });
+  });
+  rabbitConnect((conn) => {
+    conn.createChannel((err, channel) => {
+      if (err) {
+        console.log("error");
+      }
+      const queue = id_channel;
+      channel.assertQueue(queue, { durable: true });
       channel.consume(
         queue,
         function (msg) {
@@ -17,24 +26,14 @@ const sendMessageRabbit = ({ id_channel, msg, res, queryMySql }) => {
           var buf = JSON.parse(msg.content);
           //insert to database
           queryMySql(buf);
-          channel.close(() => {
-            conn.close();
-          });
         },
         { noAck: true }
       );
+      channel.close(() => {
+        conn.close();
+      });
     });
   });
-  // rabbitConnect((conn) => {
-  //   conn.createChannel((err, channel) => {
-  //     if (err) {
-  //       console.log("error");
-  //     }
-  //     const queue = id_channel;
-  //     channel.assertQueue(queue, { durable: true });
-
-  //   });
-  // });
 };
 
 export default sendMessageRabbit;
