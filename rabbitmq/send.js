923b162dce656979121f1ad960b1dd4734af4a6e
steveen <israel.delarosa@bodytechcorp.com>
2022-07-12 18:43:24 -0500

    close channle false

--- rabbitmq/send.js
+++ rabbitmq/send.js
@@ -12,10 +12,10 @@ const sendMessageRabbit = ({ id_channel, msg, queryMySql }) => {
       channel.assertQueue(queue, { durable: true });
       channel.sendToQueue(queue, message, { persistent: true });
     });
-    setTimeout(() => {
-      conn.close();
-      // process.exit(0);
-    }, 500);
+    // setTimeout(() => {
+    //   conn.close();
+    //   // process.exit(0);
+    // }, 500);
   });
   // rabbitConnect((conn) => {
   //   conn.createChannel((err, channel) => {
