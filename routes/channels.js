a3d1cc04b31a447f304874f262ad51e10dc2788e
EdinNicolas-web <edin_albarracin@outlook.com>
2022-07-13 12:12:23 -0500

    fix

--- routes/channels.js
+++ routes/channels.js
@@ -53,7 +53,7 @@ channel.post("/", async (req, response) => {
                             let channel = {
                               id_channel: channelId,
                               create_at: time,
-                              id_member: resultMember[0].id,
+                              id_member: resultMember[0].id_member,
                               id_service_line: idServiceLine,
                               id_user: idUserAsignet ?? id_user.toString(),
                               flag: "insert_channel",
